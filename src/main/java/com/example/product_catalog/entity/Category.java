17602a2ff2be8eb0250729854fd09880055be0ab
Rithvik <kollisairithvik@gmail.com>
2025-07-04 15:24:29 +0530

    Added implementation to Product and Category

---
+++ src/main/java/com/example/product_catalog/entity/Category.java
@@ -0,0 +1,5 @@
+package com.example.product_catalog.entity;
+
+public enum Category {
+    ELECTRONICS, CLOTHING, ESSENTIALS, HOME_APPLIANCES
+}
---
+++ src/main/java/com/example/product_catalog/entity/Product.java
@@ -0,0 +1,64 @@
+package com.example.product_catalog.entity;
+
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    private double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+}
